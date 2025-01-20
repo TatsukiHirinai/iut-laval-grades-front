46dbbc5dbf8e6235da82ffc196a7b1926738bc02
Tymothé Bougeant <120392004+TatsukiHirinai@users.noreply.github.com>
2025-01-20 20:55:26 +0100

    feat: css added for listStudent, addStudent, in progress for student details (#22)

--- front/src/views/students/GetStudentView.vue
+++ front/src/views/students/GetStudentView.vue
@@ -42,17 +42,39 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Student Details</h1>
-        <div v-if="student">
-            <p><strong>Prénom :</strong> {{ student.firstName }}</p>
-            <p><strong>Nom :</strong> {{ student.lastName }}</p>
-            <p><strong>Numéro d'étudiant :</strong> {{ student.studentId }}</p>
-            <p><strong>Email :</strong> {{ student.email }}</p>
-            <p><strong>Date de naissance :</strong> {{ formatDate(student.dateOfBirth) }}</p>
+    <div class="container">
+        <div class="header">
+            <RouterLink to="/students">
+                <button>Retour</button>
+            </RouterLink>
         </div>
-        <div v-else>
+
+        <div v-if="error" class="error-message">
+            <p>{{ error }}</p>
+        </div>
+
+        <div v-else-if="isLoading">
             <p>Loading...</p>
+        </div>
+
+        <div v-else>
+            <div>
+                <p>{{ student.firstName }}</p>
+                <p>{{ student.lastName }}</p>
+                <p><strong>Numéro d'étudiant :</strong> {{ student.studentId }}</p>
+                <p><strong>Email :</strong> {{ student.email }}</p>
+                <p><strong>Date de naissance :</strong> {{ formattedDateOfBirth }}</p>
+                <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-download h-5 w-5 mr-2">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                        <polyline points="7 10 12 15 17 10"></polyline>
+                        <line x1="12" x2="12" y1="15" y2="3"></line>
+                    </svg>Relevé de notes</button>
+            </div>
+            <div>
+                Notes
+            </div>
         </div>
     </div>
 </template>
