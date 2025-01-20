975eedea21f1be0749078f1d1fb76fe51471a17a
BearyRoger2 <95912266+OldBearyRoger@users.noreply.github.com>
2025-01-20 19:09:43 +0100

    feat: added the option to add a grade

--- front/src/views/grades/AddGradePage.vue
+++ front/src/views/grades/AddGradePage.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { ref } from 'vue'
     import HeaderPlaceholder from './components/HeaderPlaceholder.vue'
     import SelectStudentDropdown from './components/SelectStudentDropdown.vue'
     import SelectCourseDropdown from './components/SelectCourseDropdown.vue'
@@ -6,7 +7,40 @@
     import SemesterInput from './components/SemesterInput.vue'
     import YearInput from './components/YearInput.vue'
     import CancelButton from './components/CancelButton.vue'
-    import AddGradeButton from './components/AddGradeButton.vue'
+
+    const student  = ref('')
+    const course   = ref('')
+    const grade    = ref('')
+    const semester = ref('')
+    const year     = ref('')
+    var   status   = ref('')
+
+    async function fetchData(student, course, grade, semester, year) {
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                student: student,
+                course: course,
+                grade: grade,
+                semester: semester,
+                year: year
+            })
+        }
+        try {
+            const response = await fetch('http://localhost:3000/api/grades', requestOptions)
+            if (!response.ok) {
+                throw new Error('Network response was not ok')
+            }
+            const data = await response.json()
+            status.value = 'success'
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error)
+            status.value = 'Input error'
+        }
+    }
 </script>
 
 <template>
@@ -17,5 +51,6 @@
     <GradeInput /><br>
     <SemesterInput /><br>
     <YearInput /><br>
-    <CancelButton /><AddGradeButton />
+    <CancelButton />
+    <button @click="fetchData()">Ajouter</button>
 </template>
