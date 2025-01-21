9d52fac8c3fbb0f9bac3f401d6b94f22122878d1
Tymothé Bougeant <tyty.bougeant@gmail.com>
2025-01-21 21:02:58 +0100

    feat: added header

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,47 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+const route = useRoute();
+const firstName = localStorage.getItem('firstName')
+const departement = localStorage.getItem('departement')
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/students">List Student</RouterLink>
-      <RouterLink to="/connection">Connexion</RouterLink>
-      <RouterLink to="/courses">Courses</RouterLink>
-    </nav>
+  <header v-if="route.name !== 'connection'" class="global-header">
+    <div class="header-container">
+      <div class="header-left">
+        <img src=".\assets\logo.png" alt="Logo" class="logo" />
+        <span class="subtitle">{{ firstName }} - {{ departement }}</span>
+      </div>
+
+      <nav class="header-nav">
+        <RouterLink to="/students" class="nav-link">
+          <span>👥 Étudiants</span>
+        </RouterLink>
+        <RouterLink to="/courses" class="nav-link">
+          <span>📚 Cours</span>
+        </RouterLink>
+        <RouterLink to="/notes" class="nav-link">
+          <span>🎓 Notes</span>
+        </RouterLink>
+        <RouterLink to="/statistics" class="nav-link">
+          <span>📊 Statistiques</span>
+        </RouterLink>
+        <RouterLink to="/connection" class="nav-link">
+          <span>↪ Déconnexion</span>
+        </RouterLink>
+      </nav>
+    </div>
+  </header>
+  <header v-if="route.name == 'connection'" class="global-header">
+    <div class="header-container">
+      <div class="header-left">
+        <img src=".\assets\logo.png" alt="Logo" class="logo" />
+        <span class="subtitle">{{ firstName }} - {{ departement }}</span>
+      </div>
+
+      <nav class="header-nav">
+      </nav>
+    </div>
   </header>
   <RouterView />
 </template>
