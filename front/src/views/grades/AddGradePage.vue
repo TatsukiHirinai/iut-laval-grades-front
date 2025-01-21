edcc5eb6cb6475cdb98da74ff109abf8d1d0a80f
BearyRoger2 <95912266+OldBearyRoger@users.noreply.github.com>
2025-01-21 18:40:41 +0100

    feat: adding grades is now functional

--- front/src/views/grades/AddGradePage.vue
+++ front/src/views/grades/AddGradePage.vue
@@ -15,14 +15,14 @@
 <template>
     <h2>Ajouter une note</h2>
     <label>Etudiant</label><br>
-    <select v-model="selectedStudent">
+    <select v-model="studentId">
         <option value="" default>Sélectionner un étudiant</option>
         <option v-for="student in studentsList" :key="student.id" :value="student.id">
             {{ student.firstName }} {{ student.lastName }}
         </option>
     </select><br>
     <label>Cours</label><br>
-    <select v-model="selectedCourse">
+    <select v-model="courseId">
         <option value="" default>Sélectionner un cours</option>
         <option v-for="course in coursesList" :key="course.id" :value="course.id">
             {{ course.name }}
@@ -35,7 +35,7 @@
     <label>Année</label><br>
     <input type="text" v-model="year" placeholder="2025"><br>
     <button @click="goToGradesPage">Annuler</button>
-    <button @click="addGrade">Ajouter</button>
+    <button @click="addGrade(studentId, courseId, grade, semester, year)">Ajouter</button>
 </template>
 
 <script>
@@ -44,8 +44,8 @@
             return {
                 studentsList: [],
                 coursesList: [],
-                selectedStudent: '',
-                selectedCourse: '',
+                studentId: '',
+                courseId: '',
                 grade: '',
                 semester: '',
                 year: ''
@@ -77,17 +77,20 @@
                     console.error('Error fetching courses list:', error)
                 }
             },
-            async addGrade(id, selectedStudent, selectedCourse, grade, semester, year) {
+            async addGrade(studentId, courseId, grade, semester, academicYear) {
                 try {
                     const requestOptions = {
                         method: 'POST',
-                        headers: {'Authorization': `Bearer ${localStorage.getItem('authToken')}`},
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+                        },
                         body: JSON.stringify({
-                            studentId: '3',
-                            courseId: '2',
-                            grade: '5',
-                            semester: 'S2',
-                            academicYear: '2024-2025'
+                            studentId: studentId,
+                            courseId: courseId,
+                            grade: grade,
+                            semester: String(semester),
+                            academicYear: String(academicYear)
                         })
                     }
                     const response = await fetch('http://localhost:3000/api/grades', requestOptions)
