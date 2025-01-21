7ff359e7eecdeaf6170440d6a2683503d0dc7a9c
BearyRoger2 <95912266+OldBearyRoger@users.noreply.github.com>
2025-01-21 22:55:59 +0100

    fix: fixed router links

--- front/src/views/grades/AddGradePage.vue
+++ front/src/views/grades/AddGradePage.vue
@@ -1,47 +1,10 @@
-<script setup>
-    import { useRouter } from 'vue-router'
-
-    const apiUrl = import.meta.env.VITE_API_URL;
-
-    const router = useRouter()
-
-    function goToGradesPage() {
-        router.push('/grades')
-    }
-</script>
-
-<template>
-    <h2>Ajouter une note</h2>
-    <label>Etudiant</label><br>
-    <select v-model="studentId">
-        <option value="" default>Sélectionner un étudiant</option>
-        <option v-for="student in studentsList" :key="student.id" :value="student.id">
-            {{ student.firstName }} {{ student.lastName }}
-        </option>
-    </select><br>
-    <label>Cours</label><br>
-    <select v-model="courseId">
-        <option value="" default>Sélectionner un cours</option>
-        <option v-for="course in coursesList" :key="course.id" :value="course.id">
-            {{ course.name }}
-        </option>
-    </select><br>
-    <label>Note</label><br>
-    <input type="number" v-model="grade" placeholder="0"><br>
-    <label>Semestre</label><br>
-    <input type="text" v-model="semester" placeholder="S1, S2, etc."><br>
-    <label>Année</label><br>
-    <input type="text" v-model="year" placeholder="2025"><br>
-    <button @click="goToGradesPage">Annuler</button>
-    <button @click="addGrade(studentId, courseId, grade, semester, year)">Ajouter</button>
-</template>
-
 <script>
+    const apiUrl = import.meta.env.VITE_API_URL
     export default {
         data() {
             return {
-                studentsList: [],
-                coursesList: [],
+                students: [],
+                courses: [],
                 studentId: '',
                 courseId: '',
                 grade: '',
@@ -49,32 +12,47 @@
                 year: ''
             }
         },
-        async mounted() {
-            this.fetchStudentsList(),
-            this.fetchCoursesList()
+        async fetchStudents() {
+            try {
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+                    }
+                }
+                const response = await fetch('http://'+apiUrl+'/api/students', requestOptions)
+                if (!response.ok) {
+                    throw new Error('Network error')
+                }
+                const data = await response.json()
+                this.students = data
+                console.log(this.students)
+            } catch (error) {
+                console.error('Error fetching student list:', error)
+            }
+        },
+        async fetchCourses() {
+            try {
+                const requestOptions = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+                    }
+                }
+                const response = await fetch('http://'+apiUrl+'/api/courses', requestOptions)
+                if (!response.ok) {
+                    throw new Error('Network error')
+                }
+                const data = await response.json()
+                this.courses = data
+                console.log(this.courses)
+            } catch (error) {
+                console.error('Error fetching courses list:', error)
+            }
         },
         methods: {
-            async fetchStudentsList() {
-                try {
-                    const requestOptions = {method: 'GET', headers: {'Authorization': `Bearer ${localStorage.getItem('authToken')}`}}
-                    const response = await fetch('http://'+apiUrl+'/api/students', requestOptions)
-                    const data = await response.json()
-                    this.studentsList = data
-                } catch (error) {
-                    console.error('Error fetching student list:', error)
-                }
-            },
-            async fetchCoursesList() {
-                try {
-                    const requestOptions = {method: 'GET', headers: {'Authorization': `Bearer ${localStorage.getItem('authToken')}`}}
-                    const response = await fetch('http://'+apiUrl+'/api/courses', requestOptions)
-                    const data = await response.json()
-                    this.coursesList = data
-                    this.id = data.length
-                } catch (error) {
-                    console.error('Error fetching courses list:', error)
-                }
-            },
             async addGrade(studentId, courseId, grade, semester, academicYear) {
                 try {
                     const requestOptions = {
@@ -87,16 +65,15 @@
                             studentId: studentId,
                             courseId: courseId,
                             grade: grade,
-                            semester: String(semester),
-                            academicYear: String(academicYear)
+                            semester: semester,
+                            academicYear: academicYear
                         })
                     }
                     const response = await fetch('http://'+apiUrl+'/api/grades', requestOptions)
-                    if (!response.ok) console.log('HTTP Error :', response.status)
-                    else {
-                        const data = await response.json()
-                        if (data) console.log('Grade added successfully')
+                    if (!response.ok) {
+                        throw new Error('Network error')
                     }
+                    console.log('Grade added successfully')
                 } catch (error) {
                     console.error('Error adding grade:', error)
                 }
@@ -104,3 +81,31 @@
         }
     }
 </script>
+
+<template>
+    <h2>Ajouter une note</h2>
+    <label>Etudiant</label><br>
+    <select v-model="studentId">
+        <option value="" default>Sélectionner un étudiant</option>
+        <option v-for="student in students" :key="student.id" :value="student.id">
+            {{ student.firstName }} {{ student.lastName }}
+        </option>
+    </select><br>
+    <label>Cours</label><br>
+    <select v-model="courseId">
+        <option value="" default>Sélectionner un cours</option>
+        <option v-for="course in courses" :key="course.id" :value="course.id">
+            {{ course.name }}
+        </option>
+    </select><br>
+    <label>Note</label><br>
+    <input type="number" v-model="grade" placeholder="0"><br>
+    <label>Semestre</label><br>
+    <input type="text" v-model="semester" placeholder="S1, S2, etc."><br>
+    <label>Année</label><br>
+    <input type="text" v-model="year" placeholder="2025"><br>
+    <RouterLink :to="{name: 'grades'}">
+        <button>Annuler</button>
+    </RouterLink>
+    <button @click="addGrade(studentId, courseId, grade, semester, year)">Ajouter</button>
+</template>
