cc1fcad6e0f231565cec820320a5e13d673acb45
matheohero <134490234+matheohero@users.noreply.github.com>
2025-01-21 20:38:47 +0100

    Feature/railway implementation (#26)
    * feat: add config file
    
    * fix: try changing build option
    
    * fix: delete package-lock
    
    * chore: clean file
    
    * fix: remove railway.json
    
    * fix: try changing url
    
    * fix: change url
    
    * fix: put url in env var
    
    * Update ConnectionView.vue
    
    * Update ConnectionView.vue
    
    * Update ConnectionView.vue
    
    * Update ConnectionView.vue

--- front/src/views/ConnectionView.vue
+++ front/src/views/ConnectionView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 const mail = ref('');
 const mdp = ref('');
+const apiUrl = import.meta.env.VITE_API_URL;
 var status = ref('');
 
 async function fetchData(email , password) {
@@ -14,7 +15,7 @@ async function fetchData(email , password) {
 		body: JSON.stringify({email: email, password: password})
 	};
 	try {
-		const response = await fetch('http://localhost:3000/api/auth/login', requestOptions)
+		const response = await fetch('https://'+apiUrl+'/api/auth/login', requestOptions) 
 		if (!response.ok) {
 				throw new Error('Network response was not ok');
 			}
